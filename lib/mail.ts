041a2091b0926f8222398657ed0877ff9f897e66
Deyatele <deyatele@gmail.com>
2024-01-28 17:20:00 +0300

    add domain mail

--- lib/mail.ts
+++ lib/mail.ts
@@ -15,7 +15,7 @@ export const sendVereficationEmail = async (email: string, token: string) => {
     const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "mail@auth-nextjs14.site",
         to: email,
         subject: "Подтвердите свой адрес электронной почты и ввойдите в систему",
         html:`<p>Перейти <a href='${confirmLink}'>${confirmLink}</a> для входа.</p>`
@@ -26,7 +26,7 @@ export const sendPassworResetEmail = async (email: string, token: string) => {
     const resetLink = `${domain}/auth/new-password?token=${token}`;
 
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "mail@auth-nextjs14.site",
         to: email,
         subject: "Ссылка для сброса пароля",
         html:`<p>Перейдите по ссылки или скопируйте и вставьте в браузер ссылку <a href='${resetLink}'>${resetLink}</a> чтобы сбросить пароль.</p>`
